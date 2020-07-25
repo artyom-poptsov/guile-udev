6f06679fa111ff86575d26e304c7f17c36dd9651
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-07-25 19:50:42 +0300

    examples/device-listener.scm: Add to the repo

---
+++ examples/device-listener.scm
@@ -0,0 +1,22 @@
+#!/usr/bin/guile \
+-L modules -e main -s
+!#
+
+(use-modules (udev udev)
+	     (udev monitor)
+	     (udev device))
+
+(define (callback device)
+  (display "device: ")
+  (display (udev-device-get-devpath device))
+  (newline))
+
+(define (main args)
+  (let* ((udev         (make-udev))
+	 (udev-monitor (make-udev-monitor udev)))
+    (udev-monitor-add-filter! udev-monitor "usb" "usb_device")
+    (udev-monitor-set-callback! udev-monitor callback)
+    (sleep 1)
+    (udev-monitor-start-scanning! udev-monitor)
+    (while #t
+	   (sleep 1))))
