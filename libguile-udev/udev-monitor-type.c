53bd4916c5155de4bb3a1f9455c8061dd81653cf
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-07-29 19:22:21 +0300

    libguile-udev: Add copyright notices
    * libguile-udev/error.c, libguile-udev/error.h,
      libguile-udev/udev-device-func.c libguile-udev/udev-device-main.c,
      libguile-udev/udev-device-type.c libguile-udev/udev-device-type.h,
      libguile-udev/udev-func.h libguile-udev/udev-main.c,
      libguile-udev/udev-monitor-func.c libguile-udev/udev-monitor-main.c,
      libguile-udev/udev-monitor-type.c libguile-udev/udev-monitor-type.h,
      libguile-udev/udev-type.c libguile-udev/udev-type.h: Add copyright notice.

--- libguile-udev/udev-monitor-type.c
+++ libguile-udev/udev-monitor-type.c
@@ -1,3 +1,23 @@
+/* udev-monitor-type.h -- Udev monitor object implementation.
+ *
+ * Copyright (C) 2020 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+ *
+ * This file is part of Guile-Udev.
+ *
+ * Guile-Udev is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Guile-Udev is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Guile-Udev. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <libguile.h>
 #include <libudev.h>
 #include <pthread.h>
